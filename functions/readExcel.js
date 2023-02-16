8205a22667aed6de4bf924b901754f1a1af05d54
ffavfrn <ffern27744@gmail.com>
2023-02-16 08:18:57 +0700

    separate transport

--- functions/readExcel.js
+++ functions/readExcel.js
@@ -1,12 +1,20 @@
-import readXlsxFile from "read-excel-file"
+import readXlsxFile from "read-excel-file";
 
-
-function readExcel( file ) {
-    readXlsxFile(file[0]).then((rows) => {
-        // function แยกขนส่งใส่ใน ()
-        // ใน function รับ 1 paramiter
-        rows.filter()
-    })
+async function readExcel(file, transportName) {
+  try {
+    const rows = await readXlsxFile(file[0]);
+    const kerryRows = rows.filter((row) => row.some((cell) => cell === transportName));
+    if (!kerryRows.length) {
+      throw new Error(`Transport '${transportName}' not found in file`);
+    }
+    console.log(`Found data for '${transportName}':`, kerryRows);
+  } catch (err) {
+    console.error(`Error reading Excel file: ${err}`);
+  }
 }
 
-export default readExcel
+function readTransportExcel(file) {
+  readExcel(file, "Kerry"); // transport name you want to search for
+}
+
+export default readTransportExcel;
