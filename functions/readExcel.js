9deecc25579d24bd360149e1fa145860abc346a1
ffavfrn <ffern27744@gmail.com>
2023-02-16 09:24:08 +0700

    add create file

--- functions/readExcel.js
+++ functions/readExcel.js
@@ -1,4 +1,5 @@
 import readXlsxFile from "read-excel-file";
+import ExcelJS from "exceljs";
 
 async function readExcel(file, transportName) {
   try {
@@ -8,6 +9,29 @@ async function readExcel(file, transportName) {
       throw new Error(`Transport '${transportName}' not found in file`);
     }
     console.log(`Found data for '${transportName}':`, kerryRows);
+
+    // Create a new workbook
+    const workbook = new ExcelJS.Workbook();
+
+    // Add a new worksheet
+    const worksheet = workbook.addWorksheet("Kerry");
+
+    // Add the data to the worksheet
+    kerryRows.forEach((row) => {
+      worksheet.addRow(row);
+    });
+
+    // Save the workbook
+    const buffer = await workbook.xlsx.writeBuffer();
+    const blob = new Blob([buffer], { type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" });
+    const url = window.URL.createObjectURL(blob);
+    const link = document.createElement("a");
+    link.href = url;
+    link.download = "Kerry.xlsx";
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+    window.URL.revokeObjectURL(url);
   } catch (err) {
     console.error(`Error reading Excel file: ${err}`);
   }
